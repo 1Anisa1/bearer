150b64d999ebd18305e15c50a8c8e02ed9f84663
elsapet <elizabeth@bearer.sh>
2023-03-10 18:05:43 +0200

    Revert "feat(JS rules): rule for express session for static assets (#763)"
    This reverts commit cf39059befa6518d46ed62b89bee9e4bccd0bc72.

--- new/language/implementation/javascript/javascript.go
+++ new/language/implementation/javascript/javascript.go
@@ -219,10 +219,6 @@ func (*javascriptImplementation) PatternLeafContentTypes() []string {
 }
 
 func (implementation *javascriptImplementation) PatternIsAnchored(node *tree.Node) (bool, bool) {
-	if node.Type() == "pair" {
-		return false, false
-	}
-
 	parent := node.Parent()
 	if parent == nil {
 		return true, true
@@ -231,10 +227,10 @@ func (implementation *javascriptImplementation) PatternIsAnchored(node *tree.Nod
 	// Class body class_body
 	// arrow functions statement_block
 	// function statement_block
-	// method statement_block
-	unAnchored := []string{"statement_block", "class_body"}
+	// method statement_blocks
+	unAnchored := []string{"statement_blocks", "class_body", "pair"}
 
-	isUnanchored := !slices.Contains(unAnchored, parent.Type())
+	isUnanchored := !slices.Contains(unAnchored, node.Type())
 	return isUnanchored, isUnanchored
 }
 
@@ -243,12 +239,8 @@ func (implementation *javascriptImplementation) IsRootOfRuleQuery(node *tree.Nod
 }
 
 func (implementation *javascriptImplementation) PatternNodeTypes(node *tree.Node) []string {
-	if node.Type() == "statement_block" && node.Parent().Type() == "program" {
-		if node.NamedChildCount() == 0 {
-			return []string{"object"}
-		} else {
-			return []string{node.Type(), "program"}
-		}
+	if node.Type() == "statement_block" && node.Parent().Type() == "program" && node.NamedChildCount() == 0 {
+		return []string{"object"}
 	}
 
 	return []string{node.Type()}
